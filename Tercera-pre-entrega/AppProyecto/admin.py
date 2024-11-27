33e124feb42aa9b732cd1f634290138d51f0a0db
Tomas Harris <tomasharris9766@gmail.com>
2024-11-26 23:43:49 -0300

    Fin del proyecto

--- Tercera-pre-entrega/AppProyecto/admin.py
+++ Tercera-pre-entrega/AppProyecto/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-
+from .models import Socios, Actividades, Profesores
 # Register your models here.
+
+admin.site.register(Socios)
+admin.site.register(Actividades)
+admin.site.register(Profesores)
+
 
