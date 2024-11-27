6df9936a55c19e7bf884cc4c487397b84715ed9a
Tomas Harris <tomasharris9766@gmail.com>
2024-11-26 22:00:32 -0300

    Agrego README

--- Tercera-pre-entrega/AppProyecto/urls.py
+++ Tercera-pre-entrega/AppProyecto/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from AppProyecto import views
 
 urlpatterns = [
-    path('inicio/', views.inicio, name='inicio'),
+    path('', views.inicio, name='inicio'),
     path('socios/', views.socios, name='socios'),
     path('profesores/', views.profesores, name='profesores'),
     path('actividades/', views.actividades, name='actividades'),
